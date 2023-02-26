0704ec86516703be8147f10af47df36f1b7868d5
skryer <stealth.pro@gmail.com>
2023-02-26 19:47:30 +0300

    Done GG

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,24 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+
+    }
 
     setRange(min, max) {
-
+        this.min=min;
+        this.max=max;
     }
 
     guess() {
-
+        this.mid=Math.ceil((this.min+this.max)/2);
+        return this.mid;
     }
 
     lower() {
-
+        this.max=this.mid;
     }
 
     greater() {
-
+        this.min=this.mid;
     }
 }
 
